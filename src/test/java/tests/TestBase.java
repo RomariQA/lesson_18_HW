e58447572a8a40ea0cd16641a19a6862d630ce3d
Maya Romanovich <mariaromanovich@yandex.ru>
2024-11-16 13:33:47 +0300

    added fixes

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,17 +1,14 @@
 package tests;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 
 public class TestBase {
@@ -31,14 +28,4 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
-    @AfterEach
-    void addAttachments() {
-//        Attach.screenshotAs("Last screenshot");
-//        if (!Configuration.browser.equals("firefox")) {
-//            Attach.pageSource();
-//            Attach.browserConsoleLogs();
-//        }
-//        Attach.addVideo();
-        closeWebDriver();
-    }
 }
