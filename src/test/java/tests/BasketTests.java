2a8e768f29f7497758458d643f078ea3943882cd
Maya Romanovich <mariaromanovich@yandex.ru>
2024-11-16 13:34:42 +0300

    added fixes

--- src/test/java/tests/BasketTests.java
+++ src/test/java/tests/BasketTests.java
@@ -1,32 +1,15 @@
 package tests;
 
 import api.BasketApi;
-import com.codeborne.selenide.Configuration;
-import helpers.Attach;
 import helpers.WithLogin;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.BasketPage;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-
-
 @Tag("REGRESS")
 @DisplayName("Корзина")
 public class BasketTests extends TestBase{
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        if (!Configuration.browser.equals("firefox")) {
-            Attach.pageSource();
-            Attach.browserConsoleLogs();
-        }
-        Attach.addVideo();
-        closeWebDriver();
-    }
 
     BasketPage basketPage = new BasketPage();
 
